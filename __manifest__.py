46d7dbb0ff3dd620c03e6f2ff4b0d1170daffabd
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-05-01 05:02:57 +0300

    Department and doctor view -> doctor belong to on depart ment

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,10 @@
     'version': '0.1',
     'depends': ['base'],
     'data': [
-        'views/hms_patient_views.xml'
-    ]
+        'views/hms_patient_views.xml',
+        'views/hms_doctor_views.xml',
+        'views/hms_department_views.xml'
+    ],
+    # 'installable': True,
+    # 'application': True,
 }
